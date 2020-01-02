c7292d2431ff037fcb9f56daa6c7a00022c466c8
axlh5253 <hcgcreew@gmail.com>
2020-01-03 01:04:01 +0700

    6.push-heroku

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const app = express()
 
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = 5000
-
-
+const port =  process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(cors())
